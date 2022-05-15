f9a33854202387dd0ac0062ddd1ab2e8586a0b93
Aakash Yadav <yaakash59.ay@gmail.com>
2022-05-15 14:21:47 +0530

    added sort feature

--- fetch-and-sort/src/App.js
+++ fetch-and-sort/src/App.js
@@ -1,9 +1,56 @@
 import './App.css';
-
+import {useEffect,useState} from "react"
 function App() {
+
+
+  const [data,setData] = useState([]);
+  
+
+  useEffect(()=>{
+    fetch("https://fakestoreapi.com/users")
+    .then(Response => Response.json())
+    .then(json => setData(json))
+   
+   
+  },[])
+
+  
+
+
+  const handelClick = (e)=>{
+    if(e){
+      let sortdata = data.sort((a,b)=>{
+        return a.name.firstname.localeCompare(b.name.firstname);
+        
+      })
+      setData([...sortdata])
+     
+    }
+    else if(!e){
+        let sortdata = data.sort((a,b)=>{
+        return b.name.firstname.localeCompare(a.name.firstname);
+        
+      })
+      setData([...sortdata])
+     
+    }
+    
+
+  }
+  
+
   return (
     <div className="App">
-        hello
+       <div className='container'>
+        {
+          data.map((e)=>{
+            return <div key={e.id}> {e.name.firstname}</div>
+          })
+        }
+        <button className='btn' onClick={()=>{handelClick(true)}}> SortAccending</button>
+        <button className='btn' onClick={()=>{handelClick(false)}}> SortDeccending</button>
+        
+       </div>
     </div>
   );
 }
